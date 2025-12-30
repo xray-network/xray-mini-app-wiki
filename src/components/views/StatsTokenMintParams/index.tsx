e260f764db642dbc73a2cc0505da55d6b9138af2
xray-robot <robot@xray.app>
2025-12-30 17:44:43 +0300

    Minting params

--- src/components/views/StatsTokenMintParams/index.tsx
+++ src/components/views/StatsTokenMintParams/index.tsx
@@ -1,6 +1,7 @@
 import { Row, Col } from "antd"
 import classNames from "classnames"
 import Text from "@/components/informers/Text"
+import { ShieldCheckIcon } from "@heroicons/react/24/outline"
 import style from "./style.module.css"
 
 const StatsTokenMintParams = () => {
@@ -11,36 +12,37 @@ const StatsTokenMintParams = () => {
         capabilities. These parameters are crucial for maintaining the integrity and functionality of the XRAY/Network
         ecosystem.
       </p>
-      <div className="xray-container pb-4">
+      <div className={style.container}>
         <Row gutter={24}>
           <Col xs={24} sm={12}>
             <div className={style.section}>
               <div className={style.sectionTitle}>Total Cap</div>
               <div className={style.sectionAmount}>
-                <span className="font-size-36">324,922,240 XRAY</span>
+                <span className={style.sectionAmountLarge}>324,922,240 XRAY</span>
               </div>
             </div>
           </Col>
           <Col xs={24} sm={12}>
             <div className={style.section}>
-              <div className={style.sectionTitle}>
-                <i className="xi xi-shield_check me-2" /> Locked
+              <div className={style.sectionTitle} style={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                <ShieldCheckIcon style={{ width: "20px", height: "20px" }} strokeWidth={2} />
+                <span>Locked</span>
               </div>
-              <div className="font-size-14 text-muted lh-15">
+              <div className={style.sectionDescr}>
                 The XRAY token is a non-inflationary cryptocurrency, meaning no new tokens will be created after the
                 initial minting
               </div>
             </div>
           </Col>
         </Row>
-        <div className="xray-line xray-line-dashed mb-4" />
+        <div className="shared-line shared-line-dashed" style={{ marginBottom: "1rem" }} />
         <Row gutter={24}>
           <Col xs={24} sm={12}>
             <div className={style.section}>
               <div className={style.sectionTitle}>Policy ID</div>
-              <div className="text-wrap-letters text-mono pe-5">
+              <div>
                 <Text
-                  value={<strong>86abe45be4d8fb2e8f28e8047d17d0ba5592f2a6c8c452fc88c2c143</strong>}
+                  value={<strong className={style.address}>86abe45be4d8fb2e8f28e8047d17d0ba5592f2a6c8c452fc88c2c143</strong>}
                   copy={"86abe45be4d8fb2e8f28e8047d17d0ba5592f2a6c8c452fc88c2c143"}
                 />
               </div>
@@ -49,16 +51,16 @@ const StatsTokenMintParams = () => {
           <Col xs={24} sm={12}>
             <div className={style.section}>
               <div className={style.sectionTitle}>Fingerprint</div>
-              <div className="text-wrap-letters text-mono pe-5">
+              <div>
                 <Text
-                  value={<strong>asset1zwa4chw9xm7xwk7g46ef94qsj28hmnd7qffhgx</strong>}
+                  value={<strong className={style.address}>asset1zwa4chw9xm7xwk7g46ef94qsj28hmnd7qffhgx</strong>}
                   copy={"asset1zwa4chw9xm7xwk7g46ef94qsj28hmnd7qffhgx"}
                 />
               </div>
             </div>
           </Col>
         </Row>
-        <div className="xray-line xray-line-dashed mb-4" />
+        <div className="shared-line shared-line-dashed" style={{ marginBottom: "1rem" }} />
         <Row gutter={24}>
           <Col xs={24} sm={12}>
             <div className={style.section}>
@@ -95,7 +97,7 @@ const StatsTokenMintParams = () => {
             </div>
           </Col>
         </Row>
-        <div className="xray-line xray-line-dashed mb-3" />
+        <div className="shared-line shared-line-dashed" style={{ marginBottom: "1rem" }} />
         <div className={style.section}>
           <div className={style.sectionTitle}>Distribution Breakdown</div>
           <div className={style.chart}>
